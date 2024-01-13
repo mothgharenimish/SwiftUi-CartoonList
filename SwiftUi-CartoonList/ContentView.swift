b42211bf953a5a53aa6fa459a11156489e23fc78
Nimish Mothghare <nimishm90@gmail.com>
2024-01-13 18:31:14 +0530

    Initial Commit

---
+++ SwiftUi-CartoonList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi-CartoonList
+//
+//  Created by Nimish Mothgare on 13/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi-CartoonList/SwiftUi_CartoonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_CartoonListApp.swift
+//  SwiftUi-CartoonList
+//
+//  Created by Nimish Mothgare on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_CartoonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
