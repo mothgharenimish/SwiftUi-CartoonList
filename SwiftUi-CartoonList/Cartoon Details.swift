fa63e416639a44c50bf44aac6bdfe31edebfb4c6
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-10 16:24:03 +0530

    Xcode update changes

--- SwiftUi-CartoonList/Cartoon Details.swift
+++ SwiftUi-CartoonList/Cartoon Details.swift
@@ -15,8 +15,46 @@ struct Cartoon_Details: View {
         ZStack {
             
             Color(UIColor(hex: "#f53b50") ?? .clear)
-                .edgesIgnoringSafeArea(.all) 
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                
+                VStack(alignment: .leading,spacing: 10) {
+                    
+                    Image(cartoon.cartoonImg!)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.top)
+                    
+                    Text(cartoon.cartoonname!)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.horizontal,15)
+                        .padding(.top,10)
+                        .foregroundStyle(.white)
+                    
+                    Text("Timing: \(cartoon.cartoondaytime ?? "")")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .padding(.horizontal,15)
+                        .foregroundStyle(.white)
+                    
+                    Text("Description")
+                        .fontWeight(.medium)
+                        .fontWeight(.bold)
+                        .padding(.horizontal,15)
+                        .foregroundStyle(.white)
+                    
+                    Text(cartoon.cartoondescript!)
+                        .fontWeight(.medium)
+                        .fontWeight(.bold)
+                        .padding(.horizontal,15)
+                        .foregroundStyle(.white)
+
+                }
+                
+            }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
