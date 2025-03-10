03eb1cb5b5fd70db34576f4c343db126ad4f04b8
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-10 13:27:51 +0530

    Xcode Update Changes

---
+++ SwiftUi-CartoonList/Cartoon Details.swift
@@ -0,0 +1,18 @@
+//
+//  Cartoon Details.swift
+//  SwiftUi-CartoonList
+//
+//  Created by Nimish Sharad Mothghare on 10/03/25.
+//
+
+import SwiftUI
+
+struct Cartoon_Details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cartoon_Details()
+}
